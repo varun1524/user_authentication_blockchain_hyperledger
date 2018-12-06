48360af1d8198d77120f591f19176dcab5688f3a
Varun Shah <varun3273@gmail.com>
2018-12-05 22:32:56 -0800

    fetch data by block hash

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,32 @@
 var express = require('express');
 var router = express.Router();
-
+let query = require('./../hyperledger/fabcar/query');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
     res.status(200).send({'user status':'user api success'});
+});
+
+router.get('/fetchblock', function(req, res, next) {
+    try{
+        console.log("FetchBlock request params: ", req.body);
+        query.findBlock(req.body.block_hash, function (query_res) {
+           console.log("findBlock: ", query_res);
+            let res_body = {
+                'data': query_res
+            };
+            if(query_res){
+                res.status(200).send(query_res);
+            }
+            else {
+                res.status(404).send({});
+            }
+        });
+    }
+    catch (e) {
+        console.error("Error in FetchBlock: ", e);
+        return res.status(404).send({'msg': 'Error in FetchBlock'})
+    }
 });
 
 router.post('/addblock', function(req, res, next) {
